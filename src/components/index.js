d7cf4ab583952faf89cbbdd84c120038edd7e06a
Khodor El Jarkas <ikhodorjarkas@gmail.com>
2025-11-12 23:42:36 +0200

    Added icons package & created folders

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// import Home from "./home";
+// import Travel from "./destinations";
+// import Navbar from "./navbar";
+// import Footer from "./footer";
+// import About from "./about";
+// import Contact from "./contact";
+
+// const components = { Home, Travel, Navbar, Footer, About, Contact };
+
+// export default components;
+
